ddb9f9018a10270ff503702eac7f3d2ecc004d14
atemengue <moafembe@gmail.com>
2024-08-27 09:35:01 +0100

    Add interactive mode and Custom Layer

---
+++ RegisShop/Components/Pages/EmployeeDetail.razor.cs
@@ -0,0 +1,25 @@
+﻿using RegisShop.Services;
+using Microsoft.AspNetCore.Components;
+using BethanysPieShopHRM.Shared.Domain;
+
+namespace RegisShop.Components.Pages
+{
+    public partial class EmployeeDetail
+    {
+        [Parameter]
+
+        public int EmployeeId { get; set; }
+
+        public Employee Employee { get; set; } = new Employee();
+
+        protected override void OnInitialized()
+        {
+           Employee = MockDataService.Employees.Single( e => e.EmployeeId == EmployeeId);
+        }
+
+        private void ChangeHolidayState()
+        {
+            Employee.IsOnHoliday = !Employee.IsOnHoliday;
+        }
+    }
+}
