f7a0195d51037a0683be28c955a307a34be87ea7
atemengue <moafembe@gmail.com>
2024-08-27 09:35:01 +0100

    Ajoutez des fichiers projet.

---
+++ RegisShop/Components/Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,17 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+using RegisShop.Services;
+
+namespace RegisShop.Components.Pages
+{
+    public partial class EmployeeOverview
+    {
+        public List<Employee> Employees { get; set; } = default!;
+        protected async override Task OnInitializedAsync()
+        {
+            await Task.Delay(2000);
+            Employees = MockDataService.Employees;
+        }
+
+      
+    }
+}
