ea84ade4d8a3ca79cc73eea1ecb9bfd4c5e722e8
atemengue <moafembe@gmail.com>
2024-08-29 09:47:57 +0100

    Working with components

---
+++ RegisShop/Components/EmployeeCard.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using BethanysPieShopHRM.Shared.Domain;
+
+namespace RegisShop.Components;
+
+public partial class EmployeeCard
+{
+    [Parameter]
+    public Employee Employee { get; set; } = default!;
+}
