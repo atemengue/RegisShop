2625e4c9841d0549f0328a488ffd2a7a38c15eaa
atemengue <moafembe@gmail.com>
2024-08-29 10:58:45 +0100

    merge all

--- RegisShop/Components/Pages/EmployeeOverview.razor.cs
+++ RegisShop/Components/Pages/EmployeeOverview.razor.cs
@@ -6,10 +6,17 @@ namespace RegisShop.Components.Pages
     public partial class EmployeeOverview
     {
         public List<Employee> Employees { get; set; } = default!;
+        private Employee? _selectedEmployee;
+
         protected async override Task OnInitializedAsync()
         {
             await Task.Delay(2000);
             Employees = MockDataService.Employees;
+        }
+
+        public void ShowQuickViewPopup(Employee selectedEmployee)
+        {
+            _selectedEmployee = selectedEmployee;
         }
 
       
