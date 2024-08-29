b195f5df720b19475e27167b2626a5a140227dc2
atemengue <moafembe@gmail.com>
2024-08-29 10:58:45 +0100

    Add Interactive Modemerge all

---
+++ RegisShop/Archives/QuickViewPopup.razor.cs
@@ -0,0 +1,17 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace RegisShop.Archives
+{
+
+    public partial class QuickViewPopup
+    {
+        [Parameter]
+        public Employee? Employee { get; set; }
+
+        private Employee? _employee;
+
+    }
+
+}
+
