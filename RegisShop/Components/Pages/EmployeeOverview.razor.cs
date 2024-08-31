28e0c7915d293fe3e0f54cf8d0b8b43e162be8da
atemengue <moafembe@gmail.com>
2024-08-31 09:17:27 +0100

    Add title

--- RegisShop/Components/Pages/EmployeeOverview.razor.cs
+++ RegisShop/Components/Pages/EmployeeOverview.razor.cs
@@ -8,6 +8,8 @@ namespace RegisShop.Components.Pages
         public List<Employee> Employees { get; set; } = default!;
         private Employee? _selectedEmployee;
 
+        private string Title = "Employee overview";
+
         protected async override Task OnInitializedAsync()
         {
             await Task.Delay(2000);
